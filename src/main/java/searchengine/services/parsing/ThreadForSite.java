8dc10ea768073b4cd38bbfb666faec1799ed101d
Grigory87 <dyakonoff.grigory@yandex.ru>
2023-01-24 10:43:59 +0300

    changed code not related to work

--- src/main/java/searchengine/services/parsing/ThreadForSite.java
+++ src/main/java/searchengine/services/parsing/ThreadForSite.java
@@ -20,25 +20,12 @@ public class ThreadForSite extends Thread {
         System.out.println("Start parsing " + site.getUrl() + " " + new Date());
 
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        forkJoinPool.execute(new WalkSiteLink(site, pageParser));
+        forkJoinPool.invoke(new WalkSiteLink(site, pageParser));
 
-        while(isAlive()) {
-            if (isInterrupted()) {
-                forkJoinPool.shutdownNow();
-                pageParser.updateStatus(site, StatusType.FAILED, "Индексация прервана");
-            }
-            if (forkJoinPool.getActiveThreadCount() == 0) {
-                finishParsing(start);
-                break;
-            }
-        }
-    }
-
-    private void finishParsing(long start) {
+        pageParser.updateStatus(site, StatusType.INDEXED);
         System.out.println("Duration of site parsing "
                 + site.getUrl() + " "
                 + Tools.getTime((System.currentTimeMillis() - start) / 1000));
-
-        pageParser.updateStatus(site, StatusType.INDEXED);
     }
+    
 }
